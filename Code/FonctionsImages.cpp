2b6bccea05ceb392d46092a0ec2afe5f1e11c825
Akkuun <mathisduban@gmail.com>
2025-03-29 17:03:50 +0100

    suppresion commentaire SLIC

--- Code/FonctionsImages.cpp
+++ Code/FonctionsImages.cpp
@@ -25,24 +25,24 @@ void SLICC(int argc, char *argv[]) {
     int m = 60; //résolution spatiale
     int N = img.getSize();
 
-//    // Courbe PSNR
-//    cout << "Debut Courbe PSNR" << endl;
-//
-//    cout << "Fin Courbe PSNR" << endl;
-//
-//    //SLICC
-//    imgLAB.SLICC(k, m, N, contour);
-//    imgLAB.write(outputFilename);
-//    Image imgOUT = imgLAB.LABtoRGB();
-//    //écriture de l'image Superpixels
-//    imgOUT.write(outputFilename);
-//    cout << "Fin SLICC" << endl;
-//    Image imgOUTLAB = imgOUT.RGBtoLAB();
-//    //création de la courbe de distortion pour afficher le PSNR en fonction de nBit lors de la compression par quantification d'espace de chrominnance
-//    if (compress) {
-//        img.genererCourbePSNR(imgLAB,img,k, 10, 50, N);
-//
-//    }
+    // Courbe PSNR
+    cout << "Debut Courbe PSNR" << endl;
+
+    cout << "Fin Courbe PSNR" << endl;
+
+    //SLICC
+    imgLAB.SLICC(k, m, N, contour);
+    imgLAB.write(outputFilename);
+    Image imgOUT = imgLAB.LABtoRGB();
+    //écriture de l'image Superpixels
+    imgOUT.write(outputFilename);
+    cout << "Fin SLICC" << endl;
+    Image imgOUTLAB = imgOUT.RGBtoLAB();
+    //création de la courbe de distortion pour afficher le PSNR en fonction de nBit lors de la compression par quantification d'espace de chrominnance
+    if (compress) {
+        img.genererCourbePSNR(imgLAB,img,k, 10, 50, N);
+
+    }
 
     // Perform Mean Shift Segmentation
     float spatial_radius = 10.0f;
