9729e127bb925516fb4d22905b463fde71374b04
Akkuun <mathisduban@gmail.com>
2025-03-30 11:19:18 +0200

    changement de l'apel du programme

--- Code/FonctionsImages.cpp
+++ Code/FonctionsImages.cpp
@@ -4,57 +4,62 @@
 using namespace std;
 
 void SLICC(int argc, char *argv[]) {
-    if (argc != 5) {
+    if (argc != 7) {
         cout << "Usage: " << argv[0]
-             << " NomImageIn.ppm NomImageOutSLICC.ppm GénérerImageContour? (0/1) GénérerImageCompressé?( 0/1) " << endl;
+             << " NomImageIn.ppm SLICC? (0/1) GénérerImageContour SLICC? (0/1) GénérerImageCompressé? (0/1) Mean Shift Clustering? (0/1) GénérerImageContour Mean Shift Clustering? (0/1)"
+             << endl;
         exit(1);
     }
 
-    string inputFilename = argv[1];
-    string outputFilename = argv[2];
-    bool contour = atoi(argv[3]);
-    string outputFilenameTurboPixel = outputFilename.substr(0, outputFilename.find_last_of('.')) + "_TurboPIXEL.ppm";
-    bool compress = atoi(argv[4]);
+    string nomFichierEntree = argv[1]; // Nom de l'image d'entrée
+    //créer une string qui met SLIC juste avant le .
+    bool genererSLICC = atoi(argv[2]); // SLICC ou pas
+    bool contourSLICC = atoi(argv[3]); // Générer image contour SLICC ou pas
+    bool compressSLICC = atoi(argv[4]); // Générer image compressée SLICC ou pas
+    bool genererMeanShift = atoi(argv[5]); // Générer image contour Mean Shift ou pas
+    bool contourMeanShift = atoi(argv[6]); // Générer image contour Mean Shift ou pas
+
+
+
     //récupération image PPM
-    Image img(inputFilename, Image::PPM);
+    Image img(nomFichierEntree, Image::PPM);
     img.read();
-    //conversion en LAB
-    Image imgLAB = img.RGBtoLAB();
-    imgLAB.write(outputFilename);
-    int k = 5000; // Nombre de clusters
-    int m = 60; //résolution spatiale
-    int N = img.getSize();
 
-    // Courbe PSNR
-    cout << "Debut Courbe PSNR" << endl;
+    if (genererSLICC) {
+        Image imgLAB = img.RGBtoLAB();
+        imgLAB.write(nomFichierEntree);
+        int k = 200; // Nombre de clusters
+        int m = 20; //résolution spatiale
+        int N = img.getSize();
+        imgLAB.SLICC(k, m, N, contourSLICC);
+        Image imgOUT = imgLAB.LABtoRGB();
+        //écriture de l'image Superpixels
+        //ajoute dans la string SLIC juste avant le . le nombre de clusters et la résolution spatiale
+        string nomFichierSortieSLICC = nomFichierEntree.substr(0, nomFichierEntree.find_last_of('.')) + "_SLICC_" + to_string(k) + "_" + to_string(m) + ".ppm";
+        imgOUT.write(nomFichierSortieSLICC);
 
-    cout << "Fin Courbe PSNR" << endl;
-
-    //SLICC
-    imgLAB.SLICC(k, m, N, contour);
-    imgLAB.write(outputFilename);
-    Image imgOUT = imgLAB.LABtoRGB();
-    //écriture de l'image Superpixels
-    imgOUT.write(outputFilename);
-    cout << "Fin SLICC" << endl;
-    Image imgOUTLAB = imgOUT.RGBtoLAB();
-    //création de la courbe de distortion pour afficher le PSNR en fonction de nBit lors de la compression par quantification d'espace de chrominnance
-    if (compress) {
-        img.genererCourbePSNR(imgLAB,img,k, 10, 50, N);
-
+        if (compressSLICC) {
+            Image imgOUTLAB = imgOUT.RGBtoLAB();
+            //création de la courbe de distortion pour afficher le PSNR en fonction de nBit lors de la compression par quantification d'espace de chrominnance
+            img.genererCourbePSNR(imgLAB, img, k, 10, 50, N);
+        }
     }
 
-    // Perform Mean Shift Segmentation
-    float spatial_radius = 10.0f;
-    float color_radius = 10.0f;
-    int max_iterations = 100;
-    Image segmentedImg = imgLAB.MeanShiftSegmentation(spatial_radius, color_radius, max_iterations);
 
-    // Convert the segmented image back to RGB
-    Image resultImg = segmentedImg.LABtoRGB();
+    cout << "Fin SLICC" << endl;
 
-    // Save the segmented image
-    resultImg.write(outputFilename);
+
+//    // Perform Mean Shift Segmentation
+//    float spatial_radius = 10.0f;
+//    float color_radius = 10.0f;
+//    int max_iterations = 100;
+//    Image segmentedImg = imgLAB.MeanShiftSegmentation(spatial_radius, color_radius, max_iterations);
+//
+//    // Convert the segmented image back to RGB
+//    Image resultImg = segmentedImg.LABtoRGB();
+//
+//    // Save the segmented image
+//    resultImg.write(outputFilename);
 
 }
 
