0f08994a301c67385a5bba424c5ff6d17f566b94
Akkuun <mathisduban@gmail.com>
2025-04-02 16:38:19 +0200

    paramtres grise en fonction du mode choisi

--- Code/main.cpp
+++ Code/main.cpp
@@ -5,6 +5,7 @@
 #include <FL/Fl_Box.H>
 #include <FL/Fl_Check_Button.H>
 #include <FL/Fl_Progress.H>
+#include <FL/Fl_Input.H>
 #include <iostream>
 #include "Image.h"
 
@@ -16,6 +17,11 @@ bool contourSLICC = false;
 bool compressSLICC = false;
 bool genererMeanShift = false;
 bool contourMeanShift = false;
+int k = 700;
+int m = 55;
+float spatial_radius = 10.0f;
+float color_radius = 10.0f;
+int max_iterations = 100;
 
 void choose_file(Fl_Widget* w, void* data) {
     Fl_File_Chooser chooser(".", "*.{ppm}", Fl_File_Chooser::SINGLE, "Choose an Image");
@@ -36,25 +42,12 @@ void process_image(Fl_Widget* w, void* data) {
         return;
     }
 
-    //si l'image n'est pas au format PPM, on la convertit
-    if (selected_file.substr(selected_file.find_last_of('.') + 1) != "ppm") {
-        cout << "Image is not in PPM format. Converting..." << endl;
-        // Convert the image to PPM format
-        Image img(selected_file, Image::PPM);
-        img.read();
-        string converted_file = selected_file.substr(0, selected_file.find_last_of('.')) + ".ppm";
-        img.write(converted_file);
-        selected_file = converted_file;
-    }
-
     Image img(selected_file, Image::PPM);
     img.read();
 
     if (genererSLICC) {
         Image imgLAB = img.RGBtoLAB();
         imgLAB.write(selected_file);
-        int k = 700; // Number of clusters
-        int m = 55; // Spatial resolution
         int N = img.getSize();
         imgLAB.SLICC(k, m, N, contourSLICC);
         Image imgOUT = imgLAB.LABtoRGB();
@@ -68,10 +61,6 @@ void process_image(Fl_Widget* w, void* data) {
     }
 
     if (genererMeanShift) {
-        float spatial_radius = 10.0f;
-        float color_radius = 10.0f;
-        int max_iterations = 100;
-
         Image imgLAB = img.RGBtoLAB();
         Image segmentedImg = imgLAB.MeanShiftSegmentation(spatial_radius, color_radius, max_iterations, contourMeanShift);
         Image resultImg = segmentedImg.LABtoRGB();
@@ -83,51 +72,68 @@ void process_image(Fl_Widget* w, void* data) {
     progress->value(100);
 }
 
-void toggle_genererSLICC(Fl_Widget* w, void*) {
+void toggle_genererSLICC(Fl_Widget* w, void* data) {
     genererSLICC = !genererSLICC;
+    Fl_Input** inputs = (Fl_Input**)data;
+    for (int i = 0; i < 2; ++i) {
+        if (genererSLICC) {
+            inputs[i]->activate();
+        } else {
+            inputs[i]->deactivate();
+        }
+    }
 }
 
-void toggle_contourSLICC(Fl_Widget* w, void*) {
-    contourSLICC = !contourSLICC;
-}
-
-void toggle_compressSLICC(Fl_Widget* w, void*) {
-    compressSLICC = !compressSLICC;
-}
-
-void toggle_genererMeanShift(Fl_Widget* w, void*) {
+void toggle_genererMeanShift(Fl_Widget* w, void* data) {
     genererMeanShift = !genererMeanShift;
-}
-
-void toggle_contourMeanShift(Fl_Widget* w, void*) {
-    contourMeanShift = !contourMeanShift;
+    Fl_Input** inputs = (Fl_Input**)data;
+    for (int i = 0; i < 2; ++i) {
+        if (genererMeanShift) {
+            inputs[i]->activate();
+        } else {
+            inputs[i]->deactivate();
+        }
+    }
 }
 
 int main(int argc, char** argv) {
-    Fl_Window* window = new Fl_Window(400, 400, "Image Processing");
+    Fl_Window* window = new Fl_Window(600, 600, "Image Processing");
 
     Fl_Button* choose_button = new Fl_Button(10, 10, 100, 30, "Choose Image");
-    Fl_Box* file_box = new Fl_Box(120, 10, 270, 30, "No file chosen");
+    Fl_Box* file_box = new Fl_Box(120, 10, 470, 30, "No file chosen");
 
     Fl_Check_Button* slicc_button = new Fl_Check_Button(10, 50, 150, 30, "Generate SLICC");
     Fl_Check_Button* contour_slicc_button = new Fl_Check_Button(10, 90, 150, 30, "Contour SLICC");
     Fl_Check_Button* compress_slicc_button = new Fl_Check_Button(10, 130, 150, 30, "Compress SLICC");
 
+    Fl_Input* k_input = new Fl_Input(300, 50, 100, 30, "k:");
+    k_input->deactivate();
+    Fl_Input* m_input = new Fl_Input(300, 90, 100, 30, "m:");
+    m_input->deactivate();
+
     Fl_Check_Button* mean_shift_button = new Fl_Check_Button(10, 170, 150, 30, "Generate Mean Shift");
     Fl_Check_Button* contour_mean_shift_button = new Fl_Check_Button(10, 210, 150, 30, "Contour Mean Shift");
 
+    Fl_Input* spatial_input = new Fl_Input(300, 170, 100, 30, "Spatial Radius:");
+    spatial_input->deactivate();
+    Fl_Input* color_input = new Fl_Input(300, 210, 100, 30, "Color Radius:");
+    color_input->deactivate();
+
     Fl_Button* process_button = new Fl_Button(10, 250, 100, 30, "Process Image");
-    Fl_Progress* progress = new Fl_Progress(120, 250, 270, 30);
+    Fl_Progress* progress = new Fl_Progress(120, 250, 470, 30);
     progress->minimum(0);
     progress->maximum(100);
 
+    Fl_Input* slicc_inputs[] = {k_input, m_input};
+    Fl_Input* mean_shift_inputs[] = {spatial_input, color_input};
+
     choose_button->callback(choose_file, file_box);
-    slicc_button->callback(toggle_genererSLICC);
-    contour_slicc_button->callback(toggle_contourSLICC);
-    compress_slicc_button->callback(toggle_compressSLICC);
-    mean_shift_button->callback(toggle_genererMeanShift);
-    contour_mean_shift_button->callback(toggle_contourMeanShift);
-    process_button->callback(process_image, progress);
+    slicc_button->callback(toggle_genererSLICC, slicc_inputs);
+    contour_slicc_button->deactivate();
+    compress_slicc_button->deactivate();
+    mean_shift_button->callback(toggle_genererMeanShift, mean_shift_inputs);
+    contour_mean_shift_button->deactivate();
+    process_button->deactivate();
 
     window->end();
     window->show(argc, argv);
