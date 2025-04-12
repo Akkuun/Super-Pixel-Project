2c32ab2d769c66b83315293d8d67627b352ddb07
Akkuun <mathisduban@gmail.com>
2025-04-12 19:10:10 +0200

    exlude somes file in gitignore

--- Code/build/CMakeFiles/MyProject.dir/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/MyProject.dir/main.cpp.o: \
- /home/mathis/Documents/M1_IMAGINE/SuperPixelProjet/Code/main.cpp \
- /usr/include/stdc-predef.h \
- /home/mathis/Documents/M1_IMAGINE/SuperPixelProjet/Code/main.h
